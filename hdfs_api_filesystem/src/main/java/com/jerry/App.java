edff1fdceac242616d66cec2625f23dbefbc4d04
jerry <568384493@qq.com>
2019-11-26 23:53:00 +0800

    测试 hadoop java api

--- hdfs_api_filesystem/src/main/java/com/jerry/App.java
+++ hdfs_api_filesystem/src/main/java/com/jerry/App.java
@@ -14,7 +14,7 @@ public class App {
         //1.获取环境变量
         Configuration configuration = new Configuration();
         //2.创建Path 路径实例
-        String dfsPath = "hdfs://192.168.1.181:8020/user/root/";//设置文件目录路径
+        String dfsPath = "hdfs://192.168.3.7:8020/input/";//设置文件目录路径
         Path pathdir = new Path(dfsPath);//设置一个目录路径
         Path file = new Path(pathdir + "/test1.txt");//在目录下创建文件
         //3.创建文件系统实例对象 fs
@@ -24,12 +24,12 @@ public class App {
         //获取 fs.default.name 参数名,如当前主机 master
         System.out.println("Write to" + configuration.get("fs.default.name"));
         //将本地d盘根目录下 log.txt 文件复制到 HDFS 上 dfspath指定目录下
-        fs.copyFromLocalFile(new Path("d://log.txt"), file);
+        fs.copyFromLocalFile(false, true, new Path("d://log.txt"), file);
         //将HDFS上dfspath指定目录下的文件复制到了本地
-        fs.copyToLocalFile(file, new Path("d://"));
+        fs.copyToLocalFile(false, file, new Path("d://"), true);
 
         //文件重命名，成功返回true
-        boolean bn = fs.rename(file, new Path("hdfs://192.168.1.181:8020/user/root/renamelog.txt"));
+        boolean bn = fs.rename(file, new Path("hdfs://192.168.3.7:8020/input/renamelog.txt"));
         System.out.println(bn);
 
         //获取HDFS 集群所有节点名称信息
