077cef46c0b6a299f259c3a3621b272bc6be01a1
hcw <897796171@qq.com>
2019-12-11 16:30:48 +0800

    mapreduce

--- hdfs_api_filesystem/src/test/java/com/jerry/wordcount/AppTest.java
+++ hdfs_api_filesystem/src/test/java/com/jerry/wordcount/AppTest.java
@@ -1,4 +1,4 @@
-package com.jerry;
+package com.jerry.wordcount;
 
 import static org.junit.Assert.assertTrue;
 
---
+++ mapreduce/src/main/java/com/jerry/wordcount/WcDriver.java
@@ -0,0 +1,42 @@
+package com.jerry.wordcount;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class WcDriver {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        //1. 创建 job实例
+        Job job = Job.getInstance(new Configuration());
+
+        //2. 设置我们的类路径(classpath)
+        job.setJarByClass(WcDriver.class);
+
+        //3.设置mapper 和reducer
+        job.setMapperClass(WcMapper.class);
+        job.setReducerClass(WcReducer.class);
+
+        //4. 设置mapper 和 reducer的输出类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        //5. 设置输入输出数据
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        // 提交我们的job
+        boolean result = job.waitForCompletion(true);
+        System.exit(result ? 0 : 1);
+
+    }
+}
