762b595c6508dc6d957b0f79c3148309dcec0fc8
hcw <897796171@qq.com>
2019-12-11 17:49:09 +0800

    序列化

---
+++ mapreduce/src/main/java/com/jerry/flow/FlowBean.java
@@ -0,0 +1,52 @@
+package com.jerry.flow;
+
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class FlowBean implements Writable {
+
+    private long upFlow;
+    private long downFlow;
+    private long sumFlow;
+
+    public FlowBean() {
+    }
+
+
+    public long getUpFlow() {
+        return upFlow;
+    }
+
+    public void setUpFlow(long upFlow) {
+        this.upFlow = upFlow;
+    }
+
+    public long getDownFlow() {
+        return downFlow;
+    }
+
+    public void setDownFlow(long downFlow) {
+        this.downFlow = downFlow;
+    }
+
+    public long getSumFlow() {
+        return sumFlow;
+    }
+
+    public void setSumFlow(long sumFlow) {
+        this.sumFlow = sumFlow;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
+}
---
+++ mapreduce/src/main/java/com/jerry/flow/FlowReducer.java
@@ -0,0 +1,4 @@
+package com.jerry.flow;
+
+public class FlowReducer {
+}
